99d0687fa45da8adb4e70b1b5495dbec60e35886
Renan Mendes Carvalho <aitherios@gmail.com>
2026-09-11 19:32:06 +0200

    linter: fix linter errors
    Signed-off-by: Renan Mendes Carvalho <aitherios@gmail.com>

--- Formula/crispasr.rb
+++ Formula/crispasr.rb
@@ -1,29 +1,25 @@
 class Crispasr < Formula
   desc "C++ ggml runtime for multilingual ASR and TTS"
   homepage "https://github.com/CrispStrobe/CrispASR"
-  license "MIT"
   version "0.8.32"
+  license "MIT"
 
-  on_macos do
+  if OS.mac?
     url "https://github.com/CrispStrobe/CrispASR/releases/download/v0.8.32/crispasr-macos.tar.gz"
     sha256 "5e740d35e91a8dcaa79efd3ef0be3412de4796b68066921a9ea6984d2fc6b2ad"
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/CrispStrobe/CrispASR/releases/download/v0.8.32/crispasr-linux-arm64.tar.gz"
-      sha256 "eb39ca1274084add172764ce638a600e50fc4b65f4b18776aa78dcf31486570c"
-    else
-      url "https://github.com/CrispStrobe/CrispASR/releases/download/v0.8.32/crispasr-linux-x86_64-cpu-legacy.tar.gz"
-      sha256 "039ee3523cd1293e86e29f6395829ac4509e23733631d84f909735f34e227e48"
-    end
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/CrispStrobe/CrispASR/releases/download/v0.8.32/crispasr-linux-arm64.tar.gz"
+    sha256 "eb39ca1274084add172764ce638a600e50fc4b65f4b18776aa78dcf31486570c"
+  elsif OS.linux?
+    url "https://github.com/CrispStrobe/CrispASR/releases/download/v0.8.32/crispasr-linux-x86_64-cpu-legacy.tar.gz"
+    sha256 "039ee3523cd1293e86e29f6395829ac4509e23733631d84f909735f34e227e48"
   end
 
   def install
     bin.install "crispasr"
     bin.install "crispasr-quantize"
 
-    on_macos do
+    if OS.mac?
       lib.install "libc2pa_c.dylib"
 
       # Fix the dynamic library path so crispasr finds libc2pa_c in the Homebrew lib dir
@@ -37,9 +33,7 @@ class Crispasr < Formula
       system "codesign", "--force", "--sign", "-", "#{bin}/crispasr"
       system "codesign", "--force", "--sign", "-", "#{bin}/crispasr-quantize"
       system "codesign", "--force", "--sign", "-", "#{lib}/libc2pa_c.dylib"
-    end
-
-    on_linux do
+    elsif OS.linux?
       lib.install "libc2pa_c.so"
       lib.install Dir["lib*.so.*"]
     end
--- Formula/crispembed.rb
+++ Formula/crispembed.rb
@@ -1,22 +1,18 @@
 class Crispembed < Formula
   desc "C++ ggml runtime for embeddings, retrieval, OCR, and document understanding"
   homepage "https://github.com/CrispStrobe/CrispEmbed"
-  license "MIT"
   version "0.17.10"
+  license "MIT"
 
-  on_macos do
+  if OS.mac?
     url "https://github.com/CrispStrobe/CrispEmbed/releases/download/v0.17.10/crispembed-macos-arm64.tar.gz"
     sha256 "f6188e21f7d104c1c649c9e7090484906477f05ec7935d3f32275575c7467ef6"
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/CrispStrobe/CrispEmbed/releases/download/v0.17.10/crispembed-linux-arm64.tar.gz"
-      sha256 "00efc064958517afc16f90d7219c5f7d33037e01008b8abed9492de245bdac7c"
-    else
-      url "https://github.com/CrispStrobe/CrispEmbed/releases/download/v0.17.10/crispembed-linux-x86_64.tar.gz"
-      sha256 "dbc6eda8ad5901bd91bb8ed982c158376aa1abed2cfdc93a444f248019894477"
-    end
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/CrispStrobe/CrispEmbed/releases/download/v0.17.10/crispembed-linux-arm64.tar.gz"
+    sha256 "00efc064958517afc16f90d7219c5f7d33037e01008b8abed9492de245bdac7c"
+  elsif OS.linux?
+    url "https://github.com/CrispStrobe/CrispEmbed/releases/download/v0.17.10/crispembed-linux-x86_64.tar.gz"
+    sha256 "dbc6eda8ad5901bd91bb8ed982c158376aa1abed2cfdc93a444f248019894477"
   end
 
   def install
@@ -24,7 +20,7 @@ class Crispembed < Formula
     bin.install "crispembed-server"
     bin.install "crispembed-quantize"
 
-    on_macos do
+    if OS.mac?
       lib.install Dir["*.dylib"]
       include.install Dir["include/*"]
 
@@ -54,9 +50,7 @@ class Crispembed < Formula
       [*bin.children, *lib.glob("*.dylib")].each do |f|
         system "codesign", "--force", "--sign", "-", f.to_s
       end
-    end
-
-    on_linux do
+    elsif OS.linux?
       lib.install Dir["*.so*"]
       include.install Dir["include/*"]
     end
